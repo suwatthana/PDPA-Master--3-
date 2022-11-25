b5dd91886945e45b8ca9396e97c58dc03708485d
suwatthana <91415783+suwatthana@users.noreply.github.com>
2022-11-25 11:24:58 +0700

    first commit

---
+++ pdpa-master/src/gatsby-theme-andy/components/Footer.js
@@ -0,0 +1,32 @@
+/** @jsx jsx */
+import React from 'react'; // eslint-disable-line no-unused-vars
+import { Styled, jsx, Box } from 'theme-ui';
+
+import ReferredBlock from './ReferredBlock';
+
+const Footer = ({ references }) => {
+  return (
+    <Box p={3} sx={{ borderRadius: 2 }} mb={2} bg="accent" color="text-light">
+      <ReferredBlock references={references} />
+      <p sx={{ m: 0, fontSize: 1 }}>
+        หากพบข้อผิดพลาด หรือต้องการเสนอแนะ กรุณาติดต่อศูนย์{' '}
+        <Styled.a
+          sx={{ textDecoration: 'underline', color: 'text-light' }}
+          href="https://si.mahidol.ac.th/data/contact"
+        >
+          SiData+
+        </Styled.a>{' '}
+        หรือ{' '}
+        <Styled.a
+          sx={{ textDecoration: 'underline', color: 'text-light' }}
+          href="https://github.com/sidataplus/pdpa"
+        >
+          GitHub
+        </Styled.a>{' '}
+        นี้
+      </p>
+    </Box>
+  );
+};
+
+export default Footer;
